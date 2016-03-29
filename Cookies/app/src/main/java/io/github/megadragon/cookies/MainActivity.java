d15f24780e0eb223d50441f66413420e597c0bdf
Sergey <SSGlobov@gmail.com>
2016-03-29 18:19:18 +0300

    Cookies
    An app for testing setText() and setImageResource() functions.

---
+++ Cookies/app/src/main/java/io/github/megadragon/cookies/MainActivity.java
@@ -0,0 +1,33 @@
+package io.github.megadragon.cookies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // Find a reference to the ImageView in the layout. Change the image.
+        ImageView image = (ImageView) findViewById(R.id.android_cookie_image_view);
+        image.setImageResource(R.drawable.after_cookie);
+
+        // Find a reference to the TextView in the layout. Change the text.
+        TextView text = (TextView) findViewById(R.id.status_text_view);
+        text.setText("I'm so full");
+
+        // Disable a button was been clicked
+        ((Button) view).setEnabled(false);
+    }
+}
